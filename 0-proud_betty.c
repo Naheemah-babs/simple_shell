892551f948423386ff1b7a69d68eeab9d69bd8e6
Naheemah-babs <babatundenaheemot@gmail.com>
2023-04-26 15:48:11 -0700

    0-make betty proud

---
+++ 0-proud_betty.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * sub - subtracts two numbers
+ * @val1: first integer
+ * @val2: second integer
+ *
+ * Return: the difference between val1 and val2
+ */
+
+int sub(int val1, int val2)
+{
+/* Subtract the two values */
+return (val1 - val2);
+}
